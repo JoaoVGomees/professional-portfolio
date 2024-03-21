1c358713c756618de09e70ae65190fe8d189cab0
João <joaovitorsenac7817@gmail.com>
2024-03-21 13:16:22 -0300

    feat: edit lógica de copiar email

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,14 +1,30 @@
 function copiarTexto() {
+
+  const emailContent = document.getElementById('email-article');
+  const msg = document.createElement('p');
+
+  
+
   var texto = document.getElementById("email");
   var selecionador = document.createRange();
   selecionador.selectNode(texto);
   window.getSelection().removeAllRanges();
   window.getSelection().addRange(selecionador);
 
+
+
+
   try {
       var sucesso = document.execCommand('copy');
       var mensagem = sucesso ? 'E-mail copiado!' : 'Não foi possível copiar o texto.';
-      alert(mensagem);
+
+      msg.innerHTML = mensagem;
+      emailContent.appendChild(msg);
+
+      setTimeout(function () {
+        emailContent.removeChild(msg)
+      }, 3000);
+
   } catch (err) {
       console.log('Erro ao copiar o E-mail: ', err);
   }
